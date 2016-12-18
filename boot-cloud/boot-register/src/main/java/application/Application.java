17cdd8ec3683b9aa69ea51f79f2f3b8aa2785705
zwc00zwc <1484659036@qq.com>
2016-12-18 22:26:25 +0800

    cloud

---
+++ boot-cloud/boot-register/src/main/java/application/Application.java
@@ -0,0 +1,10 @@
+package application;
+
+/**
+ * Created by Administrator on 2016/12/18.
+ */
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class);
+    }
+}
