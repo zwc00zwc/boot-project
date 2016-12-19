0585b246738e0654d50760124983e29a0c2a906e
zwc00zwc <z09024931>
2016-12-19 13:41:16 +0800

    boot-cloud

--- boot-cloud/boot-register/src/main/java/application/Application.java
+++ boot-cloud/boot-register/src/main/java/application/Application.java
@@ -1,8 +1,14 @@
 package application;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * Created by Administrator on 2016/12/18.
  */
+@EnableEurekaServer
+@SpringBootApplication
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class);
