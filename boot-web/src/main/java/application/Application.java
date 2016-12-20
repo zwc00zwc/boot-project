8718385b7d29872ae258dda095b2ff68bf035aa8
zwc00zwc <z09024931>
2016-12-20 13:25:37 +0800

    war

--- boot-web/src/main/java/application/Application.java
+++ boot-web/src/main/java/application/Application.java
@@ -3,16 +3,27 @@ package application;
 import application.listener.MqListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Date;
 
 /**
  * Created by XR on 2016/12/9.
  */
 @SpringBootApplication
 @ComponentScan(basePackages = "core.domain,application")
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected org.springframework.boot.builder.SpringApplicationBuilder configure(org.springframework.boot.builder.SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
     public static void main(String[] args){
-        SpringApplication.run(Application.class);
+        SpringApplication springApplication= new SpringApplication(Application.class);
+        springApplication.run(args);
+        System.out.print(new Date().toString());
         MqListener.listen();
     }
 }
