443a099976fbbc8e6f19161ceac1fc24546950ae
zhengwenchao <zheng.wenchao@yrw.com>
2017-05-10 15:14:13 +0800

    elasticjob

--- boot-elasticjob/src/main/java/jobs/config/DataTestJobConfig.java
+++ boot-elasticjob/src/main/java/jobs/config/DataTestJobConfig.java
@@ -3,6 +3,7 @@ package jobs.config;
 import com.dangdang.ddframe.job.api.dataflow.DataflowJob;
 import com.dangdang.ddframe.job.config.JobCoreConfiguration;
 import com.dangdang.ddframe.job.config.dataflow.DataflowJobConfiguration;
+import com.dangdang.ddframe.job.event.JobEventConfiguration;
 import com.dangdang.ddframe.job.lite.api.JobScheduler;
 import com.dangdang.ddframe.job.lite.config.LiteJobConfiguration;
 import com.dangdang.ddframe.job.lite.spring.api.SpringJobScheduler;
@@ -23,7 +24,7 @@ public class DataTestJobConfig {
     private ZookeeperRegistryCenter zookeeperRegistryCenter;
 
     @Resource
-//    private JobEventConfiguration jobEventConfiguration;
+    private JobEventConfiguration jobEventConfiguration;
 
     @Bean
     public DataflowJob dataflowJob() {
@@ -33,7 +34,7 @@ public class DataTestJobConfig {
     @Bean(initMethod = "init")
     public JobScheduler dataflowJobScheduler(final DataflowJob dataflowJob, @Value("${dataflowJob.cron}") final String cron, @Value("${dataflowJob.shardingTotalCount}") final int shardingTotalCount,
                                              @Value("${dataflowJob.shardingItemParameters}") final String shardingItemParameters) {
-        return new SpringJobScheduler(dataflowJob, zookeeperRegistryCenter, getLiteJobConfiguration(dataflowJob.getClass(), cron, shardingTotalCount, shardingItemParameters));
+        return new SpringJobScheduler(dataflowJob, zookeeperRegistryCenter, getLiteJobConfiguration(dataflowJob.getClass(), cron, shardingTotalCount, shardingItemParameters),jobEventConfiguration);
     }
 
     private LiteJobConfiguration getLiteJobConfiguration(final Class<? extends DataflowJob> jobClass, final String cron, final int shardingTotalCount, final String shardingItemParameters) {
---
+++ boot-elasticjob/src/main/java/jobs/config/JobEventConfig.java
@@ -0,0 +1,23 @@
+package jobs.config;
+
+import com.dangdang.ddframe.job.event.JobEventConfiguration;
+import com.dangdang.ddframe.job.event.rdb.JobEventRdbConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+/**
+ * Created by alan.zheng on 2017/5/10.
+ */
+@Configuration
+public class JobEventConfig {
+    @Resource
+    private DataSource dataSource;
+
+    @Bean
+    public JobEventConfiguration jobEventConfiguration() {
+        return new JobEventRdbConfiguration(dataSource);
+    }
+}
