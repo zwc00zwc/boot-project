c6ed2f366974b5a79108d9aa702a13df4a6467e1
zhengwenchao <zheng.wenchao@yrw.com>
2017-06-14 15:14:44 +0800

    elasticjob

--- boot-elasticjob/src/main/java/jobs/DataTestJob.java
+++ boot-elasticjob/src/main/java/jobs/DataTestJob.java
@@ -4,6 +4,8 @@ import com.dangdang.ddframe.job.api.ShardingContext;
 import com.dangdang.ddframe.job.api.dataflow.DataflowJob;
 import core.domain.model.Member;
 import core.domain.service.MemberService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Date;
@@ -13,6 +15,7 @@ import java.util.List;
  * Created by alan.zheng on 2017/4/18.
  */
 public class DataTestJob implements DataflowJob {
+    private static Logger logger = LoggerFactory.getLogger(TestJob.class);
     @Autowired
     private MemberService memberService;
     //分割数据
@@ -25,9 +28,11 @@ public class DataTestJob implements DataflowJob {
     }
     //处理数据
     public void processData(ShardingContext shardingContext, List list) {
+        logger.info("分片【"+shardingContext.getShardingItem()+"】开始执行");
         if (list!=null&&list.size()>0){
             for (Object l:list) {
                 Member m = (Member) l;
+                logger.info("分片【"+shardingContext.getShardingItem()+"】执行" + m.getUserName()+new Date());
                 System.out.print(m.getUserName()+new Date());
             }
         }
--- boot-elasticjob/src/main/java/jobs/TestJob.java
+++ boot-elasticjob/src/main/java/jobs/TestJob.java
@@ -2,12 +2,16 @@ package jobs;
 
 import com.dangdang.ddframe.job.api.ShardingContext;
 import com.dangdang.ddframe.job.api.simple.SimpleJob;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by alan.zheng on 2017/4/18.
  */
 public class TestJob implements SimpleJob {
+    private static Logger logger = LoggerFactory.getLogger(TestJob.class);
     public void execute(ShardingContext shardingContext) {
+        logger.info("运行TestJob");
         System.out.print("运行TestJob");
     }
 }
